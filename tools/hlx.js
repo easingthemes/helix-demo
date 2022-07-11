6cb4cc72bf5ccbe18cead6ced6da96aa1630ce0f
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2022-07-11 14:53:00 +0200

    [INIT] #1 Change drive id

--- tools/hlx.js
+++ tools/hlx.js
@@ -1,3 +1,3 @@
-const hlx = require('@adobe/helix-cli');
+const hlx = require('/Users/draganfilipovic/.nvm/versions/node/v16.13.1/lib/node_modules/@adobe/helix-cli');
 
 console.log('@adobe/helix-cli', hlx);
